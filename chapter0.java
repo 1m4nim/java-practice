0d1cf0ce6cdb2456fe381eb728b3e6802387f54a
1m4nim <fancustard9@gmail.com>
2026-01-06 20:18:45 +0900

    変数まで終わった

---
+++ chapter0.java
@@ -0,0 +1,17 @@
+// public class chapter0 {
+//     public static void main(String[]args)
+//     {
+//         System.out.println("Hello,World");
+//     }
+// }
+
+public class chapter0{
+    public static void main(String[]args){
+        System.out.println("1m4nim");
+        System.out.println("25歳です");
+        System.out.println(31+31);
+        int x;
+        x=5;
+        System.out.println(x*x);
+    }
+}
