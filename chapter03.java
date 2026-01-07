97ea1ed581bba20fdc3aab9325e9da23be44c8a9
1m4nim <fancustard9@gmail.com>
2026-01-07 17:36:25 +0900

    switch文2

--- chapter03.java
+++ chapter03.java
@@ -18,19 +18,40 @@
 //     }
 // }
 
+// public class chapter03{
+//     public static void main(String[]args){
+//         System.out.println("占い");
+//         int fortune=1;
+//         switch(fortune){
+//             case 1:
+//                 System.out.println("大吉");
+//                 break;
+//             case 2:
+//                 System.out.println("中吉");
+//                 break;
+//             case 3:
+//                 System.out.println("小吉");
+//                 break;
+//         }
+//     }
+// }
+
 public class chapter03{
     public static void main(String[]args){
-        System.out.println("占い");
-        int fortune=1;
-        switch(fortune){
+        System.out.println("サイコロ");
+        int dice=new java.util.Random().nextInt(6)+1;
+        switch (dice){
             case 1:
-                System.out.println("大吉");
-                break;
             case 2:
-                System.out.println("中吉");
+                System.out.println("Happy");
                 break;
             case 3:
-                System.out.println("小吉");
+            case 4:
+                System.out.println("フツー");
+                break;
+            case 5:
+            case 6:
+                System.out.println("bad");
                 break;
         }
     }
