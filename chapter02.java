b9373eb3fbf7b38fb0a3c9753acceca5fb805076
1m4nim <fancustard9@gmail.com>
2026-01-07 13:55:02 +0900

    キャスト演算子を用いた型変換

--- chapter02.java
+++ chapter02.java
@@ -1,8 +1,15 @@
+// public class chapter02{
+//     public static void main(String[]args){
+//         float f=3;
+//         double d=f;
+//         System.out.println(f);//float型の変数にint型を代入したもの
+//         System.out.println(d);//double型の変数にfloat型を代入したもの
+//     }
+// }
+
 public class chapter02{
     public static void main(String[]args){
-        float f=3;
-        double d=f;
-        System.out.println(f);//float型の変数にint型を代入したもの
-        System.out.println(d);//double型の変数にfloat型を代入したもの
+        int age=(int)3.2;
+        System.out.println(age);
     }
 }
