6e238a9ffc1791208046c4922f650461e0f1f73e
1m4nim <fancustard9@gmail.com>
2026-01-07 17:17:35 +0900

    if文(1)

---
+++ chapter03.java
@@ -0,0 +1,10 @@
+public class chapter03 {
+    public static void main(String[]args){
+        boolean tenki=true;
+        if(tenki==true){
+            System.out.println("お洗濯！");
+        }else{
+            System.out.println("TV見ます");
+        }
+    }
+}
