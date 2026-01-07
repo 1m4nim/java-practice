e48585e64ad4fc1b46aeaa6a6a1eddb0c814f3d5
1m4nim <fancustard9@gmail.com>
2026-01-07 17:20:31 +0900

    無限ループ

--- chapter03.java
+++ chapter03.java
@@ -1,10 +1,19 @@
-public class chapter03 {
+// public class chapter03 {
+//     public static void main(String[]args){
+//         boolean tenki=true;
+//         if(tenki==true){
+//             System.out.println("お洗濯！");
+//         }else{
+//             System.out.println("TV見ます");
+//         }
+//     }
+// }
+
+public class chapter03{
     public static void main(String[]args){
-        boolean tenki=true;
-        if(tenki==true){
-            System.out.println("お洗濯！");
-        }else{
-            System.out.println("TV見ます");
+        boolean doorClose=true;
+        while (doorClose==true){
+            System.out.println("まだかなあ");
         }
     }
 }
