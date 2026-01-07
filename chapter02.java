5c3035e5ece653dc530ae449f2f88d6e4b423b68
1m4nim <fancustard9@gmail.com>
2026-01-07 14:00:22 +0900

    画面に文字を表示

--- chapter02.java
+++ chapter02.java
@@ -7,9 +7,17 @@
 //     }
 // }
 
+// public class chapter02{
+//     public static void main(String[]args){
+//         int age=(int)3.2;
+//         System.out.println(age);
+//     }
+// }
+
 public class chapter02{
     public static void main(String[]args){
-        int age=(int)3.2;
-        System.out.println(age);
+        String name="1m4nim";
+        System.out.print("I'm ");//printlnとは異なり改行しない
+        System.out.print(name);
     }
 }
