6f0eeed636d5fe594ca53c14c9a444448e1da3b3
1m4nim <fancustard9@gmail.com>
2026-01-08 10:48:17 +0900

    例外の回避

--- chapter04.java
+++ chapter04.java
@@ -1,8 +1,18 @@
-public class chapter04 {
+// public class chapter04 {
+//     public static void main(String[]args){
+//         int[] scores;
+//         scores=new int[5];
+//         int num =scores.length;
+//         System.out.println(num);
+//     }
+// }
+
+public class chapter04{
     public static void main(String[]args){
-        int[] scores;
-        scores=new int[5];
-        int num =scores.length;
-        System.out.println(num);
+        int[] scores={20,30,40,50,80};
+        int sum=scores[0]+scores[1]+scores[2]+scores[3]+scores[4];//添字は0からだよ
+        int avg=sum/scores.length;
+        System.out.println("sum:"+sum);
+        System.out.println("avg:"+avg);
     }
 }
