25e9d5891967650b9a522e601f283a51588e539a
1m4nim <fancustard9@gmail.com>
2026-01-08 11:10:18 +0900

    添字に対応した情報の利用

--- chapter04.java
+++ chapter04.java
@@ -39,15 +39,29 @@
 //     }
 // }
 
+// public class chapter04 {
+//     public static void main(String[] args) {
+//         int[] scores = { 20, 30, 40, 50, 80 };
+//         int count = 0;
+//         for (int i = 0; i < scores.length; i++) {
+//             if (scores[i] >= 50) {
+//                 count++;
+//             }
+//         }
+//         System.out.println("50以上は" + count);
+//     }
+// }
+
 public class chapter04 {
     public static void main(String[] args) {
-        int[] scores = { 20, 30, 40, 50, 80 };
-        int count = 0;
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] >= 50) {
-                count++;
-            }
+        int[] seq = new int[10];
+
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = new java.util.Random().nextInt(4);
         }
-        System.out.println("50以上は" + count);
+        for (int i = 0; i < seq.length; i++) {
+            String[] base = { "A", "T", "G", "C" };
+            System.out.print(base[seq[i]] + " ");
+        }
     }
 }
