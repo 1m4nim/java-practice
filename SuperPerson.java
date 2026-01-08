c1a56700cc10a9ec2578ad9541fa390df206159b
1m4nim <fancustard9@gmail.com>
2026-01-09 08:56:46 +0900

    継承(2)

--- SuperPerson.java
+++ SuperPerson.java
@@ -1,3 +1,17 @@
+// public class SuperPerson extends Person {
+//     boolean flying;
+
+//     public void fly() {
+//         this.flying = true;
+//         System.out.println("とんだ");
+//     }
+
+//     public void land() {
+//         this.flying = false;
+//         System.out.println("着地");
+//     }
+// }
+
 public class SuperPerson extends Person {
     boolean flying;
 
@@ -9,5 +23,9 @@ public class SuperPerson extends Person {
     public void land() {
         this.flying = false;
         System.out.println("着地");
+    }
+
+    public void run() {
+        System.out.println("撤退");
     }
 }
--- chapte10.java
+++ chapte10.java
@@ -1,6 +1,8 @@
 public class chapte10 {
 
     public static void main(String[] args) {
+        Person p = new Person();
+        p.run();
         SuperPerson sp = new SuperPerson();
         sp.run();
     }
