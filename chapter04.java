45dd4065bf811a4477416a5cb2695072c5cf4e40
1m4nim <fancustard9@gmail.com>
2026-01-08 10:52:20 +0900

    ループで全要素の利用

--- chapter04.java
+++ chapter04.java
@@ -7,12 +7,21 @@
 //     }
 // }
 
+// public class chapter04{
+//     public static void main(String[]args){
+//         int[] scores={20,30,40,50,80};
+//         int sum=scores[0]+scores[1]+scores[2]+scores[3]+scores[4];//添字は0からだよ
+//         int avg=sum/scores.length;
+//         System.out.println("sum:"+sum);
+//         System.out.println("avg:"+avg);
+//     }
+// }
+
 public class chapter04{
     public static void main(String[]args){
-        int[] scores={20,30,40,50,80};
-        int sum=scores[0]+scores[1]+scores[2]+scores[3]+scores[4];//添字は0からだよ
-        int avg=sum/scores.length;
-        System.out.println("sum:"+sum);
-        System.out.println("avg:"+avg);
+        int[]scores={20,30,40,50,80};
+        for (int i=0; i<scores.length; i++){
+            System.out.println(scores[i]);
+        }
     }
 }
