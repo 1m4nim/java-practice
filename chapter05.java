04619d28d82d7cc73417f1463a614ea12706065d
1m4nim <fancustard9@gmail.com>
2026-01-08 11:37:15 +0900

    引数複数

--- chapter05.java
+++ chapter05.java
@@ -8,14 +8,26 @@
 //     }
 // }
 
+// public class chapter05 {
+//     public static void main(String[] args) {
+//         hello("1m4nim");
+//         hello("chun");
+//         hello("talka");
+//     }
+
+//     public static void hello(String name) {
+//         System.out.println(name + "さん、こんにちは");
+//     }
+// }
+
 public class chapter05 {
     public static void main(String[] args) {
-        hello("1m4nim");
-        hello("chun");
-        hello("talka");
+        add(100, 20);
+        add(200, 50);
     }
 
-    public static void hello(String name) {
-        System.out.println(name + "さん、こんにちは");
+    public static void add(int x, int y){
+        int ans=x+y;
+        System.out.println(ans);
     }
 }
