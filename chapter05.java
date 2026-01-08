32916be14a151ae88253da3f772e0ffbfe12010b
1m4nim <fancustard9@gmail.com>
2026-01-08 11:32:45 +0900

    引数１つ

--- chapter05.java
+++ chapter05.java
@@ -1,9 +1,21 @@
+// public class chapter05 {
+//     public static void main(String[] args) {
+//         hello();
+//     }
+
+//     public static void hello() {
+//         System.out.println("Hello!");
+//     }
+// }
+
 public class chapter05 {
     public static void main(String[] args) {
-        hello();
+        hello("1m4nim");
+        hello("chun");
+        hello("talka");
     }
 
-    public static void hello() {
-        System.out.println("Hello!");
+    public static void hello(String name) {
+        System.out.println(name + "さん、こんにちは");
     }
 }
