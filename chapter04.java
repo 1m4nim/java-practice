1418b2245ab596265c8e109c6932a237f9256836
1m4nim <fancustard9@gmail.com>
2026-01-08 10:40:04 +0900

    配列(1)

---
+++ chapter04.java
@@ -0,0 +1,8 @@
+public class chapter04 {
+    public static void main(String[]args){
+        int[] scores;
+        scores=new int[5];
+        int num =scores.length;
+        System.out.println(num);
+    }
+}
