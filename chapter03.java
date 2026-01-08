da5846e14612be73b5a4a382e2dd1e5bbefbc3bf
1m4nim <fancustard9@gmail.com>
2026-01-08 10:28:50 +0900

    ネスト

--- chapter03.java
+++ chapter03.java
@@ -57,10 +57,22 @@
 //     }
 // }
 
+// public class chapter03{
+//     public static void main(String[]args){
+//         for(int i=0;i<10; i++){
+//             System.out.println("Hello");
+//         }
+//     }
+// }
+
 public class chapter03{
     public static void main(String[]args){
-        for(int i=0;i<10; i++){
-            System.out.println("Hello");
+        for (int i=1; i<10; i++){
+            for (int j=1; j<10; j++){
+                System.out.print(i*j);
+                System.out.print(" ");
+            }
+            System.out.println("");
         }
     }
 }
