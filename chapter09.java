aef87ed70357d9cd9b5808dbb519c21f336441ac
1m4nim <fancustard9@gmail.com>
2026-01-08 20:23:18 +0900

    9章おわり

--- chapter09.java
+++ chapter09.java
@@ -1,43 +1,43 @@
 // public class chapter09 {
-//     public static void main(String[] args) {
-//         Person p1;
-//         p1 = new Person();
-//         p1.hp = 100;
-//         Person p2;
-//         p2 = p1;
-//         p2.hp = 200;
-//         System.out.println(p1.hp);// 200が出力される
-//     }
+// public static void main(String[] args) {
+// Person p1;
+// p1 = new Person();
+// p1.hp = 100;
+// Person p2;
+// p2 = p1;
+// p2.hp = 200;
+// System.out.println(p1.hp);// 200が出力される
+// }
 // }
 
 // public class chapter09 {
-//     public static void main(String[] args) {
-//         Sword s = new Sword();
-//         s.name = "剣";
-//         s.damage = 10;
-//         Person p = new Person();
-//         p.name = "1m4nim";
-//         p.hp = 100;
-//         p.sword = s;
-//         System.out.println("現在の武器は" + p.sword.name);
-//     }
+// public static void main(String[] args) {
+// Sword s = new Sword();
+// s.name = "剣";
+// s.damage = 10;
+// Person p = new Person();
+// p.name = "1m4nim";
+// p.hp = 100;
+// p.sword = s;
+// System.out.println("現在の武器は" + p.sword.name);
+// }
 // }
 
-public class chapter09 {
-    public static void main(String[] args) {
-        Person p1 = new Person();
-        p1.name = "1m4nim";
-        p1.hp = 100;
-        Person p2 = new Person();
-        p2.name = "chun";
-        p2.hp = 200;
-        Wizard w = new Wizard();
-        w.name = "nekota";
-        w.hp = 50;
-        w.heal(p1);
-        w.heal(p2);
-        w.heal(p2);
-        System.out.println(p1.name + "のHPは" + p1.hp);
-        System.out.println(p2.name + "のHPは" + p2.hp);
-    }
-}
+// public class chapter09 {
+// public static void main(String[] args) {
+// Person p1 = new Person();
+// p1.name = "1m4nim";
+// p1.hp = 100;
+// Person p2 = new Person();
+// p2.name = "chun";
+// p2.hp = 200;
+// Wizard w = new Wizard();
+// w.name = "nekota";
+// w.hp = 50;
+// w.heal(p1);
+// w.heal(p2);
+// w.heal(p2);
+// System.out.println(p1.name + "のHPは" + p1.hp);
+// System.out.println(p2.name + "のHPは" + p2.hp);
+// }
+// }
