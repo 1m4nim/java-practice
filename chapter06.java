1efaa48f8e7246d43c3466f9e6f4493f7b4554d6
1m4nim <fancustard9@gmail.com>
2026-01-08 13:41:50 +0900

    ソースフアイルを分割

--- chapter06.java
+++ chapter06.java
@@ -1,18 +1,28 @@
-public class chapter06 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 2;
-        int total = tasu(a, b);
-        int delta = hiku(a, b);
-        System.out.println("足すと" + total);
-        System.out.println("引くと" + delta);
-    }
+// public class chapter06 {
+//     public static void main(String[] args) {
+//         int a = 10;
+//         int b = 2;
+//         int total = tasu(a, b);
+//         int delta = hiku(a, b);
+//         System.out.println("足すと" + total);
+//         System.out.println("引くと" + delta);
+//     }
 
+//     public static int tasu(int a, int b) {
+//         return (a + b);
+//     }
+
+//     public static int hiku(int a, int b) {
+//         return (a - b);
+//     }
+// }
+
+public class chapter06 {
     public static int tasu(int a, int b) {
         return (a + b);
     }
 
-    public static int hiku(int a, int b) {
-        return (a - b);
+    public static int hiku(int a,int b){
+        return (a-b);
     }
 }
---
+++ chapter06_.java
@@ -0,0 +1,10 @@
+public class chapter06_ {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int total = chapter06.tasu(a, b);
+        int delta = chapter06.hiku(a, b);
+        System.out.println("足すと" + total);
+        System.out.println("引くと" + delta);
+    }
+}
