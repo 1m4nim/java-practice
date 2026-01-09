857a82114907263c4563e73b83a68fbfcf9e4786
1m4nim <fancustard9@gmail.com>
2026-01-09 09:10:51 +0900

    継承(3)

--- Person.java
+++ Person.java
@@ -1,37 +1,37 @@
+// public class Person {
+//     String name;
+//     int hp;
+
+//     public void sleep() {
+//         this.hp = 100;
+//         System.out.println(this.name + "は、寝た");
+//     }
+
+//     public void sit(int sec) {
+//         this.hp += sec;
+//         System.out.println(this.name + "は、" + sec + "秒座った");
+//         System.out.println("HPが" + sec + "ポイント回復した！");
+//     }
+
+//     public void slip() {
+//         this.hp -= 5;
+//         System.out.println(this.name + "は、転んだ");
+//         System.out.println("5のダメージ");
+//     }
+
+//     public void run() {
+//         System.out.println("GAMEOVER");
+//         System.out.println("最終HPは" + this.hp + "でした");
+//     }
+// }
+
 public class Person {
     String name;
     int hp;
+    Sword sword;
 
-    public void sleep() {
-        this.hp = 100;
-        System.out.println(this.name + "は、寝た");
-    }
-
-    public void sit(int sec) {
-        this.hp += sec;
-        System.out.println(this.name + "は、" + sec + "秒座った");
-        System.out.println("HPが" + sec + "ポイント回復した！");
-    }
-
-    public void slip() {
-        this.hp -= 5;
-        System.out.println(this.name + "は、転んだ");
-        System.out.println("5のダメージ");
-    }
-
-    public void run() {
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは" + this.hp + "でした");
+    public void attack() {
+        System.out.println(this.name + "は、攻撃した");
+        System.out.println("敵に5ダメージ");
     }
 }
-
-// public class Person {
-// String name;
-// int hp;
-// Sword sword;
-
-// public void attack() {
-// System.out.println(this.name + "は、攻撃した");
-// System.out.println("敵に5ダメージ");
-// }
-// }
--- SuperPerson.java
+++ SuperPerson.java
@@ -12,20 +12,29 @@
 //     }
 // }
 
+// public class SuperPerson extends Person {
+//     boolean flying;
+
+//     public void fly() {
+//         this.flying = true;
+//         System.out.println("とんだ");
+//     }
+
+//     public void land() {
+//         this.flying = false;
+//         System.out.println("着地");
+//     }
+
+//     public void run() {
+//         System.out.println("撤退");
+//     }
+// }
+
 public class SuperPerson extends Person {
-    boolean flying;
-
-    public void fly() {
-        this.flying = true;
-        System.out.println("とんだ");
-    }
-
-    public void land() {
-        this.flying = false;
-        System.out.println("着地");
-    }
-
-    public void run() {
-        System.out.println("撤退");
+    public void attack(Fungi f) {
+        super.attack(f);
+        if (this.flying) {
+            super.attack(f);
+        }
     }
 }
